ca12211da26e64ae2e064bc95a8cb79106cc4a15
loh jonson <lorena.jonson@escola.pr.gov.br>
2026-08-28 07:36:46 -0300

    oi

---
+++ script.js
@@ -0,0 +1,13 @@
+const botao = document.queryselector("button");
+botao.addEventListener("click", botaoclicado);
+
+function botaoclicado() {
+    let texto = botao.queryselector("span");
+    texto. textContent++;
+}
+
+
+
+
+
+
